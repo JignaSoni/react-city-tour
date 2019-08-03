6f92352994d67b1871fb9e2e64e629bacf451539
Dev <devang.rojasara@gmail.com>
2019-08-03 17:40:03 +0530

    Some changes

--- src/Components/TourData.js
+++ src/Components/TourData.js
@@ -1,30 +1,46 @@
 export const tourData =[
     {
         id: 1,
-        city: "NewYourk",
-        img: "./img/happi.jpeg",
-        name: "New ",
-        info: "Give what ever info you want to give!"
+        city: "New York",
+        img: "./img/newyork.jpeg",
+        name: "the world trade center",
+        info: "The original World Trade Center was a large complex of seven buildings in Lower Manhattan, New York City, United States."
     },
+    
     {
         id: 2,
         city: "London",
-        img: "./img/pexels-photo-450597.jpeg",
-        name: "New York ",
-        info: "Give what ever info you want to give!"
+        img: "./img/London.jpeg",
+        name: "The London Eye ",
+        info: "The London Eye is a cantilevered observation wheel on the South Bank of the River Thames in London."
     },
     {
         id: 3,
-        city: "",
-        img: "./img/laptop.jpeg",
-        name: "New York Bridge",
-        info: "Give what ever info you want to give!"
+        city: "Paris",
+        img: "./img/Paris.jpeg",
+        name: "The Eiffel Tower",
+        info: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. "
     },
     {
         id: 4,
-        city: "",
-        img: "./img/portpholio.jpeg",
-        name: "New York Bridge Tour",
-        info: "Give what ever info you want to give!"
+        city: "Toronto",
+        img: "./img/Toronto.jpeg",
+        name: "Toronto Building",
+        info: "The Toronto waterfront is the lakeshore of Lake Ontario in the City of Toronto, Ontario in Canada."
+    },
+    {
+        id: 5,
+        city: "Singapore",
+        img: "./img/singapore.jpeg",
+        name: "Merlion, Singapore",
+        info: "Singapore’s national icon, the mythical Merlion with the head of a lion and the body of a fish."
+    
+    },
+    {
+        id: 6,
+        city: "New York",
+        img: "./img/Tajmahal.jpeg",
+        name: "Taj Mahal",
+        info: "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra."
     },
 ];
